b9445ab1bb36dbefd348229e77f846b3aa7f145f
Thiago <thiago.ralves02@gmail.com>
2022-01-12 20:32:46 -0300

    feat: App changed to Index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages/App';
+import ReactDOM from 'react-dom';import Index from './pages/Index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
