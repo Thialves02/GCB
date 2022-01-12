e117122633241920439fbf5d36a08dc61e4f35fe
Thiago <thiago.ralves02@gmail.com>
2022-01-12 20:33:08 -0300

    feat: register page created

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import RegisterCard from '../../components/RegisterCard/RegisterCard'
+import './Register.css'
+
+export default function Register() {
+    return (
+        <div className='register-container'>
+            <RegisterCard/>
+        </div>
+    )
+}
